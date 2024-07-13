2c8a409b0cd7fdcfe6aa11c7aa07628963b5b93e
snowball_213 <81451339+snowball3605@users.noreply.github.com>
2024-07-13 22:11:26 +0800

    Update subscribe.js

--- coming-soon/scripts/subscribe.js
+++ coming-soon/scripts/subscribe.js
@@ -2,7 +2,7 @@ document.getElementById('subscribeForm').addEventListener('submit', function(eve
     event.preventDefault();
 
     const email = document.getElementById('email').value;
-    const webhookUrl = 'https://discord.com/api/webhooks/1051878277473644554/9kZ2zVYZS7bnBa204UNSYPynMUrM4Q1NfKHtMSEYadEd8Un0VNL1TervDEfjZ-e6Mi7Y'; // 不用想了，你們外部發送的不會回傳到頻道~~
+    const webhookUrl = 'https://discord.com/api/webhooks/1261684500799422564/neiqxSD62nG8zuJRy_BOhrkWj9BqBzBRM8HYyiG4yac5CWPzaOpJnsGsmr3YQ8wsti_F'; // 不用想了，你們外部發送的不會回傳到頻道~~
 
     const request = new XMLHttpRequest();
     request.open("POST", webhookUrl);
